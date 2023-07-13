ce6af147340d67c9284719bd94951fae377e0f13
root <root@ip-172-31-4-114.ap-southeast-2.compute.internal>
2023-07-13 20:51:34 +0000

    random_word+file_create

--- create_file/test.tf
+++ create_file/test.tf
@@ -1,4 +1,16 @@
 resource "local_file" "name1" {
 	filename="/deletethefile.txt"
  	content="just terraform checking"
+
+}
+
+resource "random_string" "ran-string"{
+	length=10
+	special=true
+	override_special="<>#$"
+		
+}
+
+output "ran-string"{
+	value=random_string.ran-string[*].result
 }
